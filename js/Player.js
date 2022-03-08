08e21ac119714096760ab1476bd6f43e067e9e71
Arcadie <arcadie2klearn@gmail.com>
2022-03-08 23:50:47 +0200

    [ADD] basic canvas drawing, body resizing

--- js/Player.js
+++ js/Player.js
@@ -1,6 +1,16 @@
 export default class Player {
-    constructor({ x, y } = { x: 0, y: 0 }) {
+    constructor({ x, y, ctx } = { x: 0, y: 0, ctx: null }) {
         this.x = x;
         this.y = y;
+    }
+
+    draw(ctx = null) {
+        if (ctx) {
+            ctx.fillStyle = "red";
+            ctx.rect(this.x, this.y, 10, 10);
+            ctx.fill();
+        } else {
+            console.error("Context nor provided!");
+        }
     }
 }
--- js/main.js
+++ js/main.js
@@ -2,9 +2,19 @@ import "../css/style.css";
 import Player from "./Player";
 
 const canvas = document.querySelector("canvas");
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
 const ctx = canvas.getContext("2d");
 
 const player = new Player({
     x: 10,
     y: 10,
 });
+
+function update() {
+    player.draw(ctx);
+    requestAnimationFrame(update);
+}
+
+update();
